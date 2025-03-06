e21876ca52175d076b9610627b37528c75dde98a
Meister <r.meister@esatus.com>
2025-03-06 14:11:05 +0100

    use different name

--- examples/web-react/src/App.tsx
+++ examples/web-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { AuthorizeResult, createClient, CreateCredentialResult, ExtensionClient } from "signify-polaris-web";
+import { AuthorizeResult, createClient, CreateCredentialResult, ExtensionClient } from "signify-polaris-web-meister";
 import { FormEvent, useEffect, useState } from "react";
 
 export function App() {
